8a92f725779a48cd066b661d46c69c3e199befcb
Camillelima <128186522+CamilleLima@users.noreply.github.com>
2023-07-21 20:28:05 -0500

    aula do dia 21/07-corrigir o segundo parallax

--- bootstrap-project/scripts.js
+++ bootstrap-project/scripts.js
@@ -1,14 +1,117 @@
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+let containerA = new ProgressBar.Circle(circleA, {
+  color: '#aaa',
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: true
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#4973f2', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
 
-var bar = new ProgressBar.Circle(container, {
-    strokeWidth: 6,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#FFEA82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: null
-  });
-  
-  bar.animate(1.0);  // Number from 0.0 to 1.0
+    let value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+
+
+let containerB = new ProgressBar.Circle(circleB, {
+  color: '#aaa',
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: true
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#4973f2', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    let value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+
+let containerC = new ProgressBar.Circle(circleC, {
+  color: '#aaa',
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: true
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#4973f2', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    let value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+
+let containerD = new ProgressBar.Circle(circleD, {
+  color: '#aaa',
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: true
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#4973f2', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    let value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+
+  containerA.animate(1.0);  // Number from 0.0 to 1.0
+  containerB.animate(1.0);  // Number from 0.0 to 1.0
+  containerC.animate(1.0);  // Number from 0.0 to 1.0
+  containerD.animate(1.0);  // Number from 0.0 to 1.0
+
+
+//Parallax
+
+$('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
+
+$('#apply-content').parallax({imageSrc: 'img/pattern.png'})
+
